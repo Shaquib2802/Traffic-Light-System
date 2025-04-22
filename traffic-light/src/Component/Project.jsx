9848c454ec56e7edb0e8809f3e6a4b2f4b6d4bdd
Mohd Shaquib Siddiquie <mdshaquibsiddiquie@gmail.com>
2025-04-22 12:32:37 +0530

    Fix  Change

--- traffic-light/src/Component/Project.jsx
+++ traffic-light/src/Component/Project.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 
-const durations = {
-  green: 10,
-  red: 10,
-  yellow: 10,
-};
 
 const Project = () => {
   const [light, setLight] = useState("green");
   const [counter, setCounter] = useState(0);
+  const durations = {
+    green: 120,
+    red: 30,
+    yellow: 10,
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -23,41 +23,56 @@ const Project = () => {
         return next;
       });
     }, 1000);
+
     return () => clearInterval(interval);
   }, [light]);
 
-  console.log(":::", counter);
   const handleClick = (color) => {
     setLight(color);
     setCounter(0);
   };
+  console.log(":::", counter);
 
   const getLightClass = (color) => {
     const isActive = light === color;
     const base = "w-20 h-20 rounded-full cursor-pointer transition";
-    const colors = {
-      red: "bg-red-500 shadow-red-500",
-      yellow: "bg-yellow-400 shadow-yellow-400",
-      green: "bg-green-500 shadow-green-500",
+    const activeColors = {
+      red: "bg-red-600 ",
+      yellow: "bg-yellow-500 ",
+      green: "bg-green-500 ",
     };
-    return `${base} ${isActive ? colors[color] : "bg-gray-300"}`;
+    return `${base} ${isActive ? activeColors[color] : "bg-gray-300"}`;
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-black p-6 rounded-lg flex flex-col items-center gap-4">
-        <div
-          onClick={() => handleClick("red")}
-          className={getLightClass("red")}
-        />
-        <div
-          onClick={() => handleClick("yellow")}
-          className={getLightClass("yellow")}
-        />
-        <div
-          onClick={() => handleClick("green")}
-          className={getLightClass("green")}
-        />
+    <div className=" h-screen   ">
+      <div className="flex flex-col py-1 justify-center items-center">
+        <div className="mx-auto w-[10%] ">
+          <div className="bg-gray-600 p-6 rounded-lg   flex flex-col items-center gap-4">
+            <div
+              onClick={() => handleClick("red")}
+              className={`${getLightClass(
+                "red"
+              )} border border-t-4 border-gray-700 `}
+            />
+            <div
+              onClick={() => handleClick("yellow")}
+              className={`${getLightClass(
+                "yellow"
+              )} border border-t-4 border-gray-700 `}
+            />
+            <div
+              onClick={() => handleClick("green")}
+              className={`${getLightClass(
+                "green"
+              )} border border-t-4 border-gray-700`}
+            />
+          </div>
+        </div>
+        <div className="bg-black h-56 w-7">2</div>
+      </div>
+      <div className="absolute bottom-36 right-[47.8%] w-14 text-center   border-4  border-gray-600 p-1 bg-white rounded-sm font-bold  text-gray-600 ">
+        {counter}
       </div>
     </div>
   );
